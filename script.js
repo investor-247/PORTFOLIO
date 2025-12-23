49beb4ec5896bf2da41a4c34505583e3c37f392f
MR SIMPLE <uprincewill01@gmail.com>
2025-12-24 00:36:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav ul');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
